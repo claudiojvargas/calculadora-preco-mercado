da9b79a16cad05f5765a11db15a3409cc0c1372b
claudio vargas <claudiojcvargas@gmail.com>
2025-05-02 13:20:33 -0300

    #01-new-pwa-implementation

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'compras-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './script.js',
+  './manifest.json',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
+  );
+});
