452ac571b671ce2f9a6a6a7609ad0bcf8cae44c8
Claudio Vargas <30583023+claudiojvargas@users.noreply.github.com>
2026-02-20 17:12:43 -0300

    feat: adicionar toggle de tema na landing page

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,7 +1,9 @@
 import './pwaRegister.js';
 import { PWAInstaller } from './pwaInstaller.js';
+import { ThemeToggle } from './theme.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   lucide.createIcons();
   new PWAInstaller('installBtnLanding', 'inline-flex');
+  ThemeToggle.init();
 });
